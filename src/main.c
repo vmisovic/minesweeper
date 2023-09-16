81bdb50d5568e782a573da30833316cafdac71e6
vmisovic <vladimir.misovic03@gmail.com>
2023-09-16 02:33:52 +0200

    fixed main.c UpdateDrawFrame identation

--- src/main.c
+++ src/main.c
@@ -52,60 +52,60 @@ int main(void)
 
 void UpdateDrawFrame()
 {
-		switch (s)
-		{
-		case WELCOME:
-			if (IsKeyPressed(KEY_M))
-				s = MENU;
-			if (IsKeyPressed(KEY_SPACE))
-				s = GAME;
-			BeginDrawing();
-				ClearBackground(VIOLET);
-				DrawTextCenteredX("WELCOME", 200, 100, BLACK);
-			EndDrawing();
-			break;
-		case MENU:
-			if (IsKeyPressed(KEY_M))
-				s = GAME;
-			BeginDrawing();
-				ClearBackground(VIOLET);
-				DrawTextCenteredX("Menu", 200, 20, BLACK);
-			EndDrawing();
-			break;
-		case GAME:
-			if (IsKeyPressed(KEY_M))
-				s = MENU;
-			HandleZoom(&camera);
-			HandleMoveMap(&camera);
-			HandleClickOnField(&camera);
-			if (GetGameState() == SOLVED || GetGameState() == LOST)
-				// new game
-				if (IsKeyPressed(KEY_SPACE)){
-					if (IsGameSolved())
-						m++;
-					else
-						m--;
-					CloseGame();
-					InitGame(w, h, m);
-					ResetCamera2D(&camera);
-				}
-			// update camera to valid position
-			UpdateCamera2D(&camera);
-			// drawing
-			BeginDrawing();
-				ClearBackground(VIOLET);
-				// if (RUNNING)
-				BeginMode2D(camera);
-					DrawMatrix();
-				EndMode2D();
-				if (GetGameState() == WAIT)
-					DrawTextCenteredX("Click to start", 200, 20, BLACK);
-				DrawStats();
-				if (GetGameState() == SOLVED)
-					DrawEndMessage("You won!");
-				if (GetGameState() == LOST)
-					DrawEndMessage("Game over!");
-			EndDrawing();
-			break;
-		}
+	switch (s)
+	{
+	case WELCOME:
+		if (IsKeyPressed(KEY_M))
+			s = MENU;
+		if (IsKeyPressed(KEY_SPACE))
+			s = GAME;
+		BeginDrawing();
+			ClearBackground(VIOLET);
+			DrawTextCenteredX("WELCOME", 200, 100, BLACK);
+		EndDrawing();
+		break;
+	case MENU:
+		if (IsKeyPressed(KEY_M))
+			s = GAME;
+		BeginDrawing();
+			ClearBackground(VIOLET);
+			DrawTextCenteredX("Menu", 200, 20, BLACK);
+		EndDrawing();
+		break;
+	case GAME:
+		if (IsKeyPressed(KEY_M))
+			s = MENU;
+		HandleZoom(&camera);
+		HandleMoveMap(&camera);
+		HandleClickOnField(&camera);
+		if (GetGameState() == SOLVED || GetGameState() == LOST)
+			// new game
+			if (IsKeyPressed(KEY_SPACE)){
+				if (IsGameSolved())
+					m++;
+				else
+					m--;
+				CloseGame();
+				InitGame(w, h, m);
+				ResetCamera2D(&camera);
+			}
+		// update camera to valid position
+		UpdateCamera2D(&camera);
+		// drawing
+		BeginDrawing();
+			ClearBackground(VIOLET);
+			// if (RUNNING)
+			BeginMode2D(camera);
+				DrawMatrix();
+			EndMode2D();
+			if (GetGameState() == WAIT)
+				DrawTextCenteredX("Click to start", 200, 20, BLACK);
+			DrawStats();
+			if (GetGameState() == SOLVED)
+				DrawEndMessage("You won!");
+			if (GetGameState() == LOST)
+				DrawEndMessage("Game over!");
+		EndDrawing();
+		break;
+	}
 }
